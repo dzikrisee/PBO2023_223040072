89e7cfeca9860eb3b8e98b4f7b6dfd7bb2fdf7f5
dzikrisee <marlianawatiazzahra@gmail.com>
2024-03-06 18:06:04 +0700

    upload tugas

---
+++ PERTEMUAN_5/gelasMain.java
@@ -0,0 +1,19 @@
+package PERTEMUAN_5;
+
+public class gelasMain {
+    public static void main(String[] args) {
+        gelas g1 = new gelas("Hitam");
+        gelas g2 = new gelas("Biru");
+
+        tukarWarnagelas(g1, g2);
+        System.out.println("Warna g1:" + g1.getWarna());
+        System.out.println("Warna g2:" + g2.getWarna());
+    }
+
+    static void tukarWarnagelas(gelas g1, gelas g2) {
+        gelas gTemp = new gelas("Temp");
+        gTemp.setWarna(g1.getWarna());
+        g1.setWarna(g2.getWarna());
+        g2.setWarna(gTemp.getWarna());
+    }
+}
